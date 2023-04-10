e468cb5ad9f8726d38a9004c86adcbbee9225ba0
erfan-allahdadi <erfanallahdadi@gmail.com>
2023-04-10 23:07:07 +0430

    Creating project structure and folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// pages
+import HomePage from "./pages/HomePage/HomePage";
+import Auth from "./pages/AuthPage/Auth";
+
+
+import MusicsPage from "./pages/Musics/MusicsPage";
+import PlayListDetails from "./pages/PlayListDetails/PlayListDetails";
+import PlayList from "./pages/playList/playList";
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/musics" element={<MusicsPage />} />
+          <Route path="/playlists" element={<PlayList />} />
+          <Route path="/playlists/:id" element={<PlayListDetails />} />
+        </Routes>
+      </Router>
+
     </div>
+
   );
 }
 
